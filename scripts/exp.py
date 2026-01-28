71ebbe1811aba8ae23ff958a0020caf3d3013821
Paula Navarrete Díaz <paula@wpa054.wpanet.cs.local>
2026-01-28 13:04:57 -0500

    reduced experiments script

--- scripts/exp.py
+++ scripts/exp.py
@@ -1,8 +1,21 @@
 import numpy as np
+import pandas as pd
+import copy
+import random
+import time
+import os
+
 from fair.stats.survey import Corpus, SingleTopicSurvey
 from fair.agent import LegacyStudent
-from fair.allocation import general_yankee_swap_E, round_robin, serial_dictatorship
+from fair.allocation import (
+    general_yankee_swap_E,
+    round_robin,
+    serial_dictatorship,
+    integer_linear_program,
+)
+from fair.optimization import StudentAllocationProgram
 from fair.metrics import utilitarian_welfare, nash_welfare
+from fair.envy import EF_violations_reponses
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 
@@ -15,7 +28,7 @@ SPARSE = False
 PLOT = True
 seed = 0
 RNG = np.random.default_rng(seed)
-pref_thresh = 5
+pref_thresh = 100
 
 status_color_map = {
     1: "lightsteelblue",
@@ -41,10 +54,20 @@ status_crs_prefix_map = {
     5: ["5", "6"],
     6: ["5", "6"],
 }
+NUM_STUDENTS_PER_STATUS = {
+    1: 239,
+    2: 327,
+    3: 408,
+    4: 573,
+    5: 613,
+    6: 148,
+}
 
-survey_file = "resources/random_survey.csv"
-schedule_file = "resources/anonymized_courses.xlsx"
-mapping_file = "resources/survey_column_mapping.csv"
+# survey_file = "../resources/random_survey.csv"
+survey_file = "../resources/survey_data.csv"
+schedule_file = "../resources/anonymized_courses.xlsx"
+mapping_file = "../resources/survey_column_mapping.csv"
+csv_file_path = "../experiments/reduced_experiment_results.csv"
 
 mp = qsurvey.QMapper(mapping_file)
 qd = qsurvey.QSchedule(schedule_file)
@@ -70,148 +93,156 @@ course_cap_map = {
     crs: crs_sec_cap_map[course_map[crs]["course num"]][int(course_map[crs]["section"])]
     for crs in all_courses
 }
-students = [
+all_students = [
     student for student in students if len(student.student.preferred_courses) > 0
 ]
 
-status_mbeta_map = {}
-status_surveys_map = {}
-status_students_map = {}
-for status in range(1, 7):
-    relevant_idxs = qsurvey.get_status_relevant(
-        status, all_courses, course_map, status_crs_prefix_map
+n_responses_per_status = np.zeros(6)
+for student in all_students:
+    student_status = int(student_status_map[student])
+    n_responses_per_status[student_status - 1] += 1
+
+rates = [n_responses_per_status[i] / NUM_STUDENTS_PER_STATUS[i + 1] for i in range(6)]
+rate = min(rates)
+
+n_per_status = [round(NUM_STUDENTS_PER_STATUS[i + 1] * min(rates)) for i in range(6)]
+
+for sche in schedule:
+    sche.capacity = round(sche.capacity * rate)
+
+
+def add_experiment_result(
+    NUM_STUDENTS,
+    seed,
+    pref_thresh,
+    alg,
+    runtime,
+    X,
+    students,
+    schedule,
+    c,
+    csv_file_path,
+):
+    current_utilities = np.diag(np.dot(c, X))
+    USW = sum(current_utilities)
+    seats = sum(sum(X)[:NUM_STUDENTS])
+    zeros, nash = nash_welfare(X, students, schedule, current_utilities)
+    total_envy, status_envy, downward_envy = EF_violations_reponses(
+        X, students, schedule, student_status_map, c
     )
-    status_students = [
-        student for student in students if student_status_map[student] == status
-    ]
-    status_students_map[status] = status_students
-    status_surveys = [
-        SingleTopicSurvey(
-            [sch for i, sch in enumerate(schedule) if i in relevant_idxs],
-            student_resp_map[student][relevant_idxs],
-            student.student.total_courses,
-            1,
-            8,
-        )
-        for student in status_students
-    ]
-    status_corpus = Corpus(status_surveys, RNG)
-    status_mbeta = status_corpus.kde_distribution(SAMPLE_PER_STUDENT, NUM_SUB_KERNELS)
-    status_mbeta_map[status] = status_mbeta
-    status_surveys_map[status] = status_surveys
 
+    file_exists = os.path.isfile(csv_file_path)
 
-def project_data(status_data_map, course_map):
-    pca = PCA(n_components=2)
-    data_matrix = np.vstack(
-        [status_data_map[status] for status in qsurvey.STATUS_LABEL_MAP.keys()]
+    new_row = pd.DataFrame(
+        {
+            "NUM_STUDENTS": [NUM_STUDENTS],
+            "pref_thresh": [pref_thresh],
+            "seed": [seed],
+            "alg": [alg],
+            "USW": [USW],
+            "seats": [seats],
+            "zeros": [zeros],
+            "nash": [nash],
+            "total_envy": [total_envy],
+            "status_envy": [status_envy],
+            "downward_envy": [downward_envy],
+            "runtime": [runtime],
+        }
     )
-    # sign posts
-    import re
 
-    signs = []
-    for i in range(1, 7):
-        signs.append(
-            [
-                1 if re.match(r"^" + str(i) + r"\d{2}.*?", crs["course num"]) else 0
-                for crs in course_map.values()
-            ]
-        )
-
-    data_matrix = np.concatenate([np.vstack(signs), data_matrix])
-    data = pca.fit_transform(data_matrix)
-    sign_data = data[0:7]
-
-    proj_data_map = {}
-    start = 7
-    for status in qsurvey.STATUS_LABEL_MAP.keys():
-        stop = start + len(status_data_map[status])
-        proj_data_map[status] = data[start:stop][:]
-        start = stop
-
-    return proj_data_map, sign_data
+    if file_exists:
+        new_row.to_csv(csv_file_path, mode="a", header=False, index=False)
+    else:
+        new_row.to_csv(csv_file_path, mode="w", header=True, index=False)
 
 
-def project_data_ind(status_data_map):
-    pca = PCA(n_components=2)
-    proj_data_map = {}
-    for status in qsurvey.STATUS_LABEL_MAP.keys():
-        proj_data_map[status] = pca.fit_transform(np.vstack(status_data_map[status]))
+for seed in range(10):
+    random.seed(seed)
+    reduced_students = []
+    for status in range(1, 7):
+        students_status = [
+            student for student in all_students if student_status_map[student] == status
+        ]
+        selected_students = random.sample(students_status, n_per_status[status - 1])
+        reduced_students = [*reduced_students, *selected_students]
 
-    return proj_data_map
+    students = reduced_students
+    NUM_STUDENTS = len(students)
+    print("Num students,", NUM_STUDENTS)
 
+    students.sort(key=lambda x: student_status_map[x])
+    students.reverse()
 
-def plot_data(status, data, num_students):
-    actual_students = data[:num_students, :]
-    synthetic_students = data[num_students:, :]
-    plt.scatter(
-        synthetic_students[:, 0],
-        synthetic_students[:, 1],
-        c=status_color_map[status],
-        alpha=0.25,
-        label="_hidden",
-    )
-    plt.scatter(
-        actual_students[:, 0],
-        actual_students[:, 1],
-        c=status_color_map[status],
-        alpha=0.25,
-        s=150,
-        label=f"{qsurvey.STATUS_LABEL_MAP[status]}",
-    )
-    # plt.tick_params(labelbottom=False, labelleft=False)
+    c = np.vstack([student_resp_map[student] for student in students]) - 1
 
-
-status_synth_students_map = {}
-status_data_map = {}
-for status in qsurvey.STATUS_LABEL_MAP.keys():
-    synth_students, data = qsurvey.synthesize_students(
-        NUM_RAND_SAMP,
-        course,
-        section,
-        features,
+    print("run ILP")
+    start = time.time()
+    X_ILP = integer_linear_program(students, schedule, valuations=c)
+    runtime = time.time() - start
+    add_experiment_result(
+        NUM_STUDENTS,
+        seed,
+        pref_thresh,
+        "ILP",
+        runtime,
+        X_ILP,
+        students,
         schedule,
-        qs,
-        status_surveys_map[status],
-        status_mbeta_map[status],
-        course_map,
-        status_max_course_map[status],
-        qsurvey.get_status_relevant(
-            status, all_courses, course_map, status_crs_prefix_map
-        ),
-        rng=RNG,
-        pref_thresh=pref_thresh,
-        total_course_list=[
-            student.student.total_courses for student in status_students_map[status]
-        ],
+        c,
+        csv_file_path,
     )
-    status_synth_students_map[status] = synth_students
-    status_data_map[status] = data
-    synth_students = [
-        LegacyStudent(student, student.preferred_courses, course)
-        for student in synth_students
-    ]
-    data1 = data[-len(synth_students) :]
-    data_max = [max(val) for val in data1]
-    # print(f"max preference: {max(data_max)}")
-    for i, response in enumerate(data1):
-        student_resp_map[synth_students[i]] = [int(i) for i in response]
-    students = [*students, *synth_students]
-    for student in synth_students:
-        student_status_map[student] = status
-    print("synthetic student preferred courses", student.student.preferred_courses)
+
+    print("run SD")
+    start = time.time()
+    X_SD = serial_dictatorship(students, schedule, c)
+    runtime = time.time() - start
+    add_experiment_result(
+        NUM_STUDENTS,
+        seed,
+        pref_thresh,
+        "SD",
+        runtime,
+        X_SD,
+        students,
+        schedule,
+        c,
+        csv_file_path,
+    )
+
+    print("run RR")
+    start = time.time()
+    X_RR = round_robin(students, schedule, c)
+    runtime = time.time() - start
+    add_experiment_result(
+        NUM_STUDENTS,
+        seed,
+        pref_thresh,
+        "RR",
+        runtime,
+        X_RR,
+        students,
+        schedule,
+        c,
+        csv_file_path,
+    )
+
+    print("run YS")
+    start = time.time()
+    X_YS_1, _, _ = general_yankee_swap_E(
+        students, schedule, valuations=c
+    )
+    runtime = time.time() - start
+    add_experiment_result(
+        NUM_STUDENTS,
+        seed,
+        pref_thresh,
+        "YS",
+        runtime,
+        X_YS_1,
+        students,
+        schedule,
+        c,
+        csv_file_path,
+    )
 
 
-# proj_data_map, sign_data = project_data(status_data_map, course_map)
-
-
-X_YS, _, _ = general_yankee_swap_E(students, schedule)
-X_RR = round_robin(students, schedule)
-X_SD = serial_dictatorship(students, schedule)
-
-print("YS utilitarian welfare: ", utilitarian_welfare(X_YS, students, schedule))
-print("YS Nash welfare: ", nash_welfare(X_YS, students, schedule))
-print("RR utilitarian welfare: ", utilitarian_welfare(X_RR, students, schedule))
-print("RR Nash welfare: ", nash_welfare(X_RR, students, schedule))
-print("SD utilitarian welfare: ", utilitarian_welfare(X_SD, students, schedule))
-print("SD Nash welfare: ", nash_welfare(X_SD, students, schedule))
